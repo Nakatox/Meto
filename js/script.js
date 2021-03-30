44dcb56de561d2d3c3c62ff725d5cc6f4321acc1
Vincent <vincent.loron@supinternet.fr>
2021-03-30 13:42:23 +0200

    add function weather

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
 import * as API from './webservices'
 
+window.addEventListener('DOMContentLoaded', function(){     
 
+    
+    let button = document.querySelector('.submit')
+    let weatherInfos = []
+    let reload = document.querySelector('.submitReload')
 
-let city = API.getCityName().then((data)=>{
-    console.log(API.getWeather(data))
+    function getCity(){
+        return new Promise((resolve)=>{
+            API.getCityName().then((data)=>{
+                API.getWeather(data).then((field)=>{
+                    resolve(field)
+                })
+            })
+        })
+         
+    }   
+
+    button.addEventListener('click', function(){
+        button.style.display ="none"
+        getCity().then((data)=>{
+            showWeather(data)
+        })
+    })
+
+    reload.addEventListener('click', function(){
+        getCity().then((data)=>{
+            showWeather(data)
+        })
+    })
+
+    function showWeather(data){
+        let longitude = data.coord.lon
+        let latitude = data.coord.lat
+        let name = data.name
+        let temp = data.main.temp
+        let temp_max = data.main.temp_max
+        let temp_min = data.main.temp_min
+    }
+
 })
